9883b21a9f47647bc669047a3600fbe79e6063d5
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2021-12-12 22:15:22 +0300

    Add routes for robot moves and X-Y positions

---
+++ routes/robot.js
@@ -0,0 +1,105 @@
+import express from 'express'
+import {
+  position,
+  moveLeft,
+  moveRight,
+  moveForward,
+  moveBackward,
+  moveLeftForward,
+  moveRightForward,
+  moveLeftBackward,
+  moveRightBackward
+} from '../gcode-utils.js'
+
+const router = express.Router()
+
+router.get('/position', (req, res) => {
+  try {
+    res.status(200).json(position)
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err.message)
+  }
+})
+
+router.get('/move/left', async (req, res) => {
+  try {
+    const robotRes = await moveLeft()
+    res.status(200).json(robotRes)
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err.message)
+  }
+})
+
+router.get('/move/right', async (req, res) => {
+  try {
+    const robotRes = await moveRight()
+    res.status(200).json(robotRes)
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err.message)
+  }
+})
+
+router.get('/move/forward', async (req, res) => {
+  try {
+    const robotRes = await moveForward()
+    res.status(200).json(robotRes)
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err.message)
+  }
+})
+
+router.get('/move/backward', async (req, res) => {
+  try {
+    const robotRes = await moveBackward()
+    res.status(200).json(robotRes)
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err.message)
+  }
+})
+
+router.get('/move/left-forward', async (req, res) => {
+  try {
+    const robotRes = await moveLeftForward()
+    res.status(200).json(robotRes)
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err.message)
+  }
+})
+
+router.get('/move/right-forward', async (req, res) => {
+  try {
+    const robotRes = await moveRightForward()
+    res.status(200).json(robotRes)
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err.message)
+  }
+})
+
+router.get('/move/left-backward', async (req, res) => {
+  try {
+    const robotRes = await moveLeftBackward()
+    res.status(200).json(robotRes)
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err.message)
+  }
+})
+
+router.get('/move/right-backward', async (req, res) => {
+  try {
+    const robotRes = await moveRightBackward()
+    res.status(200).json(robotRes)
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err.message)
+  }
+})
+
+export default router
