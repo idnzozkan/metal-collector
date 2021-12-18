c43604b45970ce98cc060e098b00c6cfaca9b70c
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2021-12-18 22:15:09 +0300

    Add move to a position router

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,22 @@ import axios from 'axios'
 import { useEffect, useState } from 'react'
 
 const App = () => {
-  const [position, setPosition] = useState({})
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+  const [target, setTarget] = useState({ x: null, y: null })
   const [isSent, setIsSent] = useState(false)
+
+  const handleInput = e => {
+    if (e.target.name === 'x-value') setTarget(prev => ({ ...prev, x: e.target.value }))
+    if (e.target.name === 'y-value') setTarget(prev => ({ ...prev, y: e.target.value }))
+  }
+
+  const handleMoveToTarget = async () => {
+    try {
+      await axios.post(`http://localhost:8080/robot/move/position`, target)
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   const handleMove = async direction => {
     try {
@@ -26,7 +40,7 @@ const App = () => {
     }
     getRobotPosition()
     setIsSent(false)
-  }, [isSent])
+  }, [])
 
   return (
     <div>
@@ -40,6 +54,21 @@ const App = () => {
           <button onClick={() => handleMove('right-forward')}>RIGHT-FORWARD</button>
           <button onClick={() => handleMove('left-backward')}>LEFT-BACKWARD</button>
           <button onClick={() => handleMove('right-backward')}>RIGHT-BACKWARD</button>
+        </div>
+        <div className="specific-position-entering-area">
+          <input
+            type="number"
+            placeholder="X Value"
+            name="x-value"
+            onChange={e => handleInput(e)}
+          />
+          <input
+            type="number"
+            placeholder="Y Value"
+            name="y-value"
+            onChange={e => handleInput(e)}
+          />
+          <button onClick={handleMoveToTarget}>Move</button>
         </div>
         <div className="position">
           <p>
